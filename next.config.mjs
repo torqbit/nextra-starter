1ab198d0b07a81cbbde9a2958f2f8ab07a140083
Shad <shad.amezng@gmail.com>
2025-12-06 21:36:53 +0530

    fix(build): added the output export

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const withNextra = nextra({
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
+  output: "export",
   turbopack: {
     resolveAlias: {
       "next-mdx-import-source-file": "./mdx-components.js",
